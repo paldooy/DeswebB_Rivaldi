440c8e2fc26bb2bded7f3484b5c1bbbf6b280577
paldooy <kemoywashere@gmail.com>
2025-10-30 11:52:34 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <UserLogin v-if="!isLoggedIn" @loginSuccess="isLoggedIn = true" /><!-- SOAL: tambahkan tag html sebelum directive berupa variabel yang di import di bagian script-->
+    <UserDashboard v-else @logout="isLoggedIn = false" /><!-- SOAL: tambahkan tag html sebelum directive berupa variabel yang di import di bagian script-->
+  </div>
+</template>
+
+<script>
+import UserLogin from './components/UserLogin.vue';
+import UserDashboard from './components/UserDashboard.vue'; //SOAL: Definisikan variabel import
+
+export default {
+  name: 'App',
+  components: {
+    UserLogin,
+    UserDashboard
+    // SOAL: panggil components yang akan digunakan
+  },
+  data() {
+    return {
+      isLoggedIn: false
+    };
+  }
+};
+</script>
+
+<style>
+body {
+  font-family: Arial, sans-serif;
+}
+</style>
